24318696704df8cb45f0f71dca5e32454c13c34e
atharvingavale69-blip <atharvingavale69@gmail.com>
2026-06-01 15:18:32 +0000

    feat: configure strapi cors for lovable frontend production

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -2,7 +2,17 @@ export default [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: [
+        'http://localhost:5173',
+        'http://localhost:3000',
+        'https://friendly-cod-jrq6w6jv7gg2x76-1337.app.github.dev',
+        'https://atharv-ingavale-portfolio.lovable.app'
+      ],
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
